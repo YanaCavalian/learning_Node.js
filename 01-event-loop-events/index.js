2c9d7b0b4799cad673982713e83ff1946c3e2042
Yana <cavaljn@icloud.com>
2024-02-08 21:34:41 +0100

    code optimization

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -2,45 +2,43 @@ const fs = require("fs");
 
 const dns = require("dns");
 
-function timestamp() {
-  return performance.now().toFixed(2);
+function info(text) {
+  console.log(text, performance.now().toFixed(2));
 }
 
 console.log("Program start");
 
 //Close events
-fs.writeFile("./test.txt", "Hello Node.js", () =>
-  console.log("File written", timestamp())
-);
+fs.writeFile("./test.txt", "Hello Node.js", () => info("File written"));
 
 //Promises
-Promise.resolve().then(() => console.log("Promise 1", timestamp()));
+Promise.resolve().then(() => info("Promise 1"));
 
 //Next tick
-process.nextTick(() => console.log("NextTick 1", timestamp()));
+process.nextTick(() => info("NextTick 1"));
 
 //setImmediate (Check)
-setImmediate(() => console.log("Immediate 1", timestamp()));
+setImmediate(() => info("Immediate 1"));
 
 //Timeouts
-setTimeout(() => console.log("Timer 1", timestamp()), 0);
+setTimeout(() => info("Timer 1"), 0);
 setTimeout(() => {
-  process.nextTick(() => console.log("Next tick 2", timestamp()));
-  console.log("Timer 2", timestamp());
+  process.nextTick(() => info("Next tick 2"));
+  info("Timer 2");
 }, 100);
 
 //Intervals
 let intervalCount = 0;
 const intervalId = setInterval(() => {
-  console.log(`Interval ${(intervalCount += 1)}`, timestamp());
+  info(`Interval ${(intervalCount += 1)}`);
   if (intervalCount === 2) clearInterval(intervalId);
 }, 50);
 
 //I/O Events
 dns.lookup("localhost", (error, address, family) => {
-  console.log("DNS 1 localhost", address, timestamp());
-  Promise.resolve().then(() => console.log("Promise 2", timestamp()));
-  process.nextTick(() => console.log("Next tick 3", timestamp()));
+  info("DNS 1 localhost", address);
+  Promise.resolve().then(() => info("Promise 2"));
+  process.nextTick(() => info("Next tick 3"));
 });
 
 console.log("Program end");
