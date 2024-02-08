11260fa925ea2e15ff610214a51d8060540b7449
Yana <cavaljn@icloud.com>
2024-02-08 21:50:24 +0100

    blocked-event

---
+++ 02-blocked-event-loop/while.js
@@ -0,0 +1,11 @@
+let isRunning = true;
+
+// мы не доходим до этого события,так как цикл while неприрывный !
+// тоесть цикл событий заблокирован!
+
+setTimeout(() => (isRunning = false), 0);
+process.nextTick(() => console.log("next Tick"));
+
+while (isRunning) {
+  console.log("While loop is running...");
+}
