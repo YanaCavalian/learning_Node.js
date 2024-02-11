116b960495cc2d08d9ba92e22ad6db73c5a6fdeb
Yana <cavaljn@icloud.com>
2024-02-11 21:31:28 +0100

    changes to variable names in other modules

--- 03-commojs-modules/export-and-import.js
+++ 03-commojs-modules/export-and-import.js
@@ -1,6 +1,9 @@
-const { myName } = require('./multiple-export');
+const { myName, myHobbies } = require('./multiple-export');
 
 const myFriendName = 'Alice';
 
 module.exports.myName = myName;
 module.exports.myFriendName = myFriendName;
+
+//property names could be different from the variable names
+module.exports.myGreatHobbies = myHobbies;
--- 03-commojs-modules/index.js
+++ 03-commojs-modules/index.js
@@ -1,12 +1,23 @@
 const { myName, myHobbies, myFavoriteNumber } = require('./multiple-export');
-const { myName: myOtherName, myFriendName } = require('./export-and-import');
+const {
+  myName: myOtherName,
+  myFriendName,
+  myGreatHobbies,
+} = require('./export-and-import');
 const gretting = require('./single-export');
 
+// Imports from multiple-exports
 console.log(myName);
 console.log(myHobbies);
 console.log(myFavoriteNumber);
 
+// change array myHobbies
+myHobbies.push('climbing');
+
+// Imports from single_export
 gretting(myName);
 
+// Imports from export-and-import
 console.log(myOtherName);
 console.log(myFriendName);
+console.log(myGreatHobbies);
